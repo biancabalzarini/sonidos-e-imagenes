3b7bcd2c7b7390aec1d429cd9e0740ef92fc91f5
biancabalzarini <balzarini.bianca@gmail.com>
2023-07-13 00:16:14 -0300

    continuacion del ejercicio

--- sonidos_e_imagenes.r
+++ sonidos_e_imagenes.r
@@ -123,5 +123,104 @@ title('Habla - Volumen bajo')
 ### Parte B – Periodograma-Espectrograma
 
 ## 1) Dibuje el periodograma de cada sonido.
-##    Puede usar a = periodogram(sonido) ; plot(a)
-##    Si da error, podría usar spec(sonido) ; axis(side=2,las=2)
+
+x11() ; spec(drum) ; axis(side=2, las=2) ; title(main="Tambor")
+x11() ; a = periodogram(bird) ; plot(a) ; title(main="Pájaro")
+x11() ; a = periodogram(speech) ; plot(a) ; title(main="Habla")
+
+## 2) ¿En qué rangos se encuentran las frecuencias?
+
+# El tambor está aproximadamente entre 0kHz y 3kHz
+# El pájaro está aproximadamente entre 2kHz y 6.5kHz
+# El habla está aproximadamente entre 0kHz y 2kHz
+
+## 3) Muestre el espectrograma de cada sonido.
+
+# Dibujo el espectrograma:
+x11() ; spectro(drum) ; title(main="Tambor")
+# Dibujo la versión zoomeada para ver la parte interesante:
+x11() ; spectro(drum, flim=c(0,0.5), osc=T) ; title(main="Tambor")
+
+x11() ; spectro(bird) ; title(main="Pájaro")
+x11() ; spectro(bird, flim=c(2,8), osc=T) ; title(main="Pájaro")
+
+x11() ; spectro(speech) ; title(main="Habla")
+x11() ; spectro(speech, flim=c(0,3), osc=T) ; title(main="Habla")
+
+## 4) Considere uno de los sonido y fíltrelo con un filtro pasa-bajo y con un filtropasa-alto.
+##    Muestre el espectrograma resultante de cada sonido filtrado.
+
+# Pasa bajo
+drumPB = ffilter(bird, to=200, output="Wave")
+drumPB = normalize(drumPB, unit='16')
+x11() ; spectro(drumPB, flim=c(0,0.5)) ; title(main = "Tambor con filtro pasa bajos")
+
+# Pasa alto
+drumPA = ffilter(bird, from=200, output="Wave")
+drumPA = normalize(drumPA, unit='16')
+x11() ; spectro(drumPA, flim=c(0,0.5)) ; title(main = "Tambor con filtro pasa altos")
+
+### Parte C – Análisis
+
+## 1) Según el espectrograma de cada sonido, ¿en qué rangos se encuentran las frecuencias?
+
+# La mayor parte de la intensidad del tambor está aproximadamente entre 0kHz y 0.5kHz
+# La mayor parte de la intensidad del pájaro está aproximadamente entre 2kHz y 7kHz
+# La mayor parte de la intensidad del habla está aproximadamente entre 0kHz y 3kHz
+
+## 2) En el caso del sonido de habla, muestre el oscilograma y el espectrograma uno debajo
+##    del otro y trate de identificar dónde se está diciendo cada palabra.
+
+x11() ; spectro(speech, flim=c(0,3), osc=T) ; title(main="Habla")
+
+# No voy a poder reconocer todas las palabras porque son demasiadas, pero miro algunas secciones.
+
+# Frase "si esto sigue"
+speech_cortado = window(speech, start = 1000, end = 25000)
+play(speech_cortado)
+x11() ; spectro(speech_cortado, flim=c(0,3), osc=T) ; title(main="Habla (cortado)")
+
+# Frase "diamantes por una"
+speech_cortado = window(speech, start = 76000, end = 95000)
+play(speech_cortado)
+x11() ; spectro(speech_cortado, flim=c(0,3), osc=T) ; title(main="Habla (cortado)")
+# La palabra "diamantes" es todo un contínuo en el espectrograma.
+# Antes del la "p" del "por" se ve claramente un silencio.
+# Después del "por" hay otro claro silencio, para que aparezca al final el "una".
+
+
+## 3) En el caso del sonido de habla, trate de identificar silencios o consonantes en
+##    el espectrograma o el oscilograma.
+
+# Frase "diamantes por una"
+speech_cortado = window(speech, start = 76000, end = 95000)
+play(speech_cortado)
+x11() ; spectro(speech_cortado, flim=c(0,3), osc=T) ; title(main="Habla (cortado)")
+# Antes y después de la palabra "por" se ven claramente lo silencios.
+
+# Frase "una gallina"
+speech_cortado = window(speech, start = 92000, end = 107000)
+play(speech_cortado)
+x11() ; spectro(speech_cortado, flim=c(0,3), osc=T) ; title(main="Habla (cortado)")
+# Antes de la "g" de gallina se un silencio.
+
+# Palabra "esto"
+speech_cortado = window(speech, start = 8500, end = 14100)
+play(speech_cortado)
+x11() ; spectro(speech_cortado, flim=c(0,3), osc=T) ; title(main="Habla (cortado)")
+# Esto es interesante porque es solamente la palabra "esto", y se ve claramente un silencio entre
+# "es" y "to" por la consonante "t". La boca tiene que posicionarse para pronunciar la consonante
+# explosiva "t", y por eso se produce ese silencio que es impercetible por el oído.
+
+## 4) Compare los 3 espectrogramas y saque alguna conclusión.
+
+x11() ; spectro(drum, flim=c(0,0.5), osc=T) ; title(main="Tambor")
+x11() ; spectro(bird, flim=c(2,8), osc=T) ; title(main="Pájaro")
+x11() ; spectro(speech, flim=c(0,3), osc=T) ; title(main="Habla")
+
+# En el espectrogrma del tambor se ve claramente cada uno de los golpes del tambor, por ser un
+# sonido explosivo. El golpe es muy fuerte y luego decae con el tiempo, hasta que llega el próximo
+# golpe. Las frecuencias son bajas porque es un sonido grave.
+
+# El espectrograma del pájaro muestra frecuencias mucho más altas, lo cual tiene sentido porque
+# el canto es muy agudo. ### SEGUIIIRRRRRRRRRRRRRRRRRRRRRRRRRRRR
